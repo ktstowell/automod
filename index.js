0759a5c6b7b99177c832eacd2dc6b27726301ba7
Ken Stowell <kstowell@rubicon.com>
2015-11-06 22:27:49 -0800

    Inital tests proving symlink creation and ability to `require` a symlinked dir.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var os = require('os');
 var exec = require('child_process').exec;
 var findit = require('findit');
 var path = require('path');
+var chalk = require('chalk');
 
 /***********************************************************************************************************************************************
  * AUTO MOD
@@ -31,8 +32,8 @@ var platforms = {
  * Comand map
  * @type {Object}
  */
-var cmds = {
-  nix: function(src, dest) { return 'ln -s '+ src + ' ' + dest; },
+var commands = {
+  nix: function(src, dest) { return 'if ! [ -L "' + dest + '" ]; then ln -s ' + src + ' ' + dest + '; fi'; },
   win: function(src, dest) { return 'cp -r '+ src + ' ' + dest; }
 };
 
@@ -49,18 +50,22 @@ var cmds = {
 // if windows - will need copy
 // if *nix can use symlink
 module.exports = function(opts) {
-  var src;
+  var root;
   var platform;
-  var finder;
   var manifest;
+  var dest;
+  var logging;
+  var packages = [];
 
   // member validation
-  opts = opts || {};
-  src = opts.src || process.cwd();
+  opts = (opts = opts || {});
+  root = opts.root || process.cwd();
+  logging = opts.logging || 'debug';
   platform = os.platform();
   manifest = 'package.json';
+  dest = 'node_modules'
 
-  // Get map of generic platform
+  // Get platform settings
   for(var type in platforms) {
     if(platforms[type].indexOf(platform) !== -1) {
       platform = type;
@@ -68,16 +73,124 @@ module.exports = function(opts) {
     }
   }
 
-  // start package.json parsing
-  finder = findit(src);
+  /**
+   * Start looking for package.json/manifest
+   */
+  find({src: root, find: manifest})
+    // Upon finding:
+    .on('found', function(data) {
+
+      // Notify
+      logger(logging, 'success', 'Package found');
+      // Track found packages for return
+      packages.push(data);
+
+      // Run the command
+      var foo = command(commands[platform](data.src, root + '/' + dest + '/' +data.pckg.name))
+        .on('success', function(msg) { logger(logging, 'success', msg); })
+        .on('error', function(msg) { logger(logging, 'error', msg); })
+        .on('complete', function(msg) { logger(logging, 'complete', msg); });
+    });
+
+  return {opts: opts, platform: os.platform(), packages: packages};
+}
+
+//
+// SUPPORT
+//------------------------------------------------------------------------------------------//
+// @description
+
+/**
+ * [description]
+ * @return {[type]} [description]
+ */
+function command(cmd) {
+  var stream = exec(cmd);
+  var api;
+
+  var events = {
+    success: function() {},
+    error: function() {},
+    complete: function() {}
+  };
+
+  stream.stdout.on('data', function(data) {
+    events.success(data);
+  });
+
+  stream.stderr.on('data', function(data) {
+    events.error(data);
+  });
+
+  stream.on('close', function(data) {
+    events.complete(data);
+  });
+
+  api = {
+    on: on
+  };
+
+  return api;
+
+  function on(type, fn) {
+    events[type] = fn;
+    return api;
+  }
+}
+
+/**
+ * [find description]
+ * @param  {[type]} root [description]
+ * @return {[type]}      [description]
+ */
+function find(data, fn) {
+  var api;
+  var finder = findit(data.src);
+  var events = {
+    found: function() {}
+  };
 
   // Skip node modules/git
   finder.on('directory', function(dir, stat, stop) {
     var base = path.basename(dir);
-    if (base === '.git' || base === 'node_modules') { stop(); }
+    if (base === '.git' || base === 'node_modules') { stop(); } // make this configurable later
   });
 
+  // On file, 
   finder.on('file', function(file, stat) {
-    if(path.basename(file) === manifest)
+    var name = path.basename(file);
+    var pckg;
+    var src;
+
+    if(name === data.find) {
+      pckg = require(file);
+      src = file.split(name)[0];
+      
+      if(pckg) {
+        events.found({pckg: pckg, src: src});
+      }
+    }
   });
-};
+
+  api = {
+    on: on
+  };
+
+  return api;
+
+  function on(type, fn) {
+    events[type] = fn;
+    return api;
+  }
+}
+
+/**
+ * [log description]
+ * @param  {[type]} setting [description]
+ * @param  {[type]} type    [description]
+ * @param  {[type]} msg     [description]
+ * @return {[type]}         [description]
+ */
+function logger(setting, type, msg) {
+  console.log('Logger: ', setting, type, msg);
+}
--- spec/automod.spec.js
+++ spec/automod.spec.js
@@ -7,6 +7,7 @@ var vows = require('vows');
 var assert = require('assert');
 var suite = vows.describe('automod');
 var automod = require('..');
+
 //
 // TESTS
 //------------------------------------------------------------------------------------------//
@@ -14,10 +15,26 @@ var automod = require('..');
 suite.addBatch({
   'When using automod... ': {
     topic: function() {
-      return automod({src: __dirname});
+      var self = this,
+          mod = automod({root: __dirname});
+
+      setTimeout(function() {
+        self.callback(null, mod);
+      }, 1000);
     }, 'all package.json files should be retrieved recursively': function(mod) {
-      console.log(mod)
-      assert.equal(true, true);
+      // kind og a shitty test - but works since i know what __dirname is here.
+      assert.equal(mod.packages.length, true);
+    }
+  },
+  'Given I have a "users" module with a package.json...': {
+    topic: function() {
+      return automod({root: __dirname});
+    }, ' and I have used "automod to be able to "require" it': {
+      topic: function() {
+        return require('users');
+      }, 'I should gain access to its API from anywhere within my application simply by typing "require(users)"': function(users) {
+        console.log(users);
+      }
     }
   }
 }).export(module);
